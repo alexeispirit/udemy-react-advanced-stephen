4afbdd7615d9b49784b2196ca7f1c789a101d4dd
Alexei Spirit <alexeispirit@gmail.com>
2020-05-12 21:14:56 +0500

    39-form-submit

--- 01-testing/src/components/__tests__/CommentBox.test.js
+++ 01-testing/src/components/__tests__/CommentBox.test.js
@@ -27,3 +27,15 @@ it("has a textarea that users can type in", () => {
 
   expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
+
+it("has an empty textarea after form submit", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+  wrapped.update();
+
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
