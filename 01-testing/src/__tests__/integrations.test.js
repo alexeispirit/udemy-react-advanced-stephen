2e122a42797e722056545d8d4c9a996e51438cce
Alexei Spirit <alexeispirit@gmail.com>
2020-05-13 14:39:37 +0500

    64-introducing-pause

--- 01-testing/src/__tests__/integrations.test.js
+++ 01-testing/src/__tests__/integrations.test.js
@@ -17,17 +17,21 @@ afterEach(() => {
   moxios.uninstall();
 });
 
-it("can fetch a list of comments and display them", () => {
-  // Attempt to render the *entire* app
+it("can fetch a list of comments and display them", (done) => {
   const wrapped = mount(
     <Root>
       <App />
     </Root>
   );
 
-  // find the 'fetchComments' button and click it
   wrapped.find(".fetch-comments").simulate("click");
 
-  // Expect to find a list of comments!
-  expect(wrapped.find("li").length).toEqual(500);
+  setTimeout(() => {
+    wrapped.update();
+
+    expect(wrapped.find("li").length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  }, 100);
 });
