077a8a17904c0d0753cfb9d9896004d891fb5498
Alexei Spirit <alexeispirit@gmail.com>
2020-05-13 17:01:51 +0500

    75-changing-auth-state

--- 02-hoc/src/components/App.js
+++ 02-hoc/src/components/App.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Route, Link } from "react-router-dom";
 
+import * as actions from "actions";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
 class App extends Component {
   renderButton() {
     if (this.props.auth) {
-      return <button>Sign Out</button>;
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      );
     } else {
-      return <button>Sign In</button>;
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      );
     }
   }
 
@@ -43,4 +48,4 @@ function mapStateToProps(state) {
   return { auth: state.auth };
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, actions)(App);
