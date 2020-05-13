cdd0826943338d066f194bb09797e3f4e01c71e9
Alexei Spirit <alexeispirit@gmail.com>
2020-05-13 23:19:21 +0500

    85-forwarding-actions

--- 03-redux-middleware/src/middlewares/async.js
+++ 03-redux-middleware/src/middlewares/async.js
@@ -1,1 +1,9 @@
-export default ({ dispatch }) => (next) => (action) => {};
+export default ({ dispatch }) => (next) => (action) => {
+  // check to see if the action has a promise in payload
+  // if it does, then wait for it to resolve
+  // if it doesn't, then send the action to next middleware
+
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+};
