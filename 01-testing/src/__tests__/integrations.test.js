828323705a1e1976e1f4990f402e4085eb56b52f
Alexei Spirit <alexeispirit@gmail.com>
2020-05-13 14:23:06 +0500

    59-fixing-broken-test

--- 01-testing/src/__tests__/integrations.test.js
+++ 01-testing/src/__tests__/integrations.test.js
@@ -4,8 +4,13 @@ import { mount } from "enzyme";
 import Root from "Root";
 import App from "components/App";
 
-it("car fetch a list of comments and display them", () => {
+it("can fetch a list of comments and display them", () => {
   // Attempt to render the *entire* app
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
   // find the 'fetchComments' button and click it
   // Expect to find a list of comments!
 });
