89ff12b87b5b3822321629dd0a2b14db2650a210
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 18:23:49 +0500

    127-redux-form-for-signup

--- 04-authentication/client/src/components/SignUp.js
+++ 04-authentication/client/src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
 class SignUp extends Component {
   render() {
@@ -6,13 +7,25 @@ class SignUp extends Component {
       <form>
         <fieldset>
           <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
       </form>
     );
   }
 }
 
-export default SignUp;
+export default reduxForm({ form: "signup" })(SignUp);
--- 04-authentication/client/src/reducers/index.js
+++ 04-authentication/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 
-import auth from "./auth";
+import authReducer from "./auth";
 
 export default combineReducers({
-  auth,
+  auth: authReducer,
+  form: formReducer,
 });
