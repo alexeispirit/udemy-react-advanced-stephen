3bae133726c86f5d5481913e562d4ceddd61fb2f
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 22:27:20 +0500

    137-redirect-on-signup

--- 04-authentication/client/src/actions/index.js
+++ 04-authentication/client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { AUTH_USER, AUTH_ERROR } from "./types";
 
-export const signup = ({ email, password }) => async (dispatch) => {
+export const signup = ({ email, password }, callback) => async (dispatch) => {
   try {
     const response = await axios.post("http://localhost:3090/signup", {
       email,
@@ -13,6 +13,7 @@ export const signup = ({ email, password }) => async (dispatch) => {
       type: AUTH_USER,
       payload: response.data.token,
     });
+    callback();
   } catch (err) {
     dispatch({
       type: AUTH_ERROR,
