7f44bdda313c1fe15fa091130beaaeba7863acc0
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 09:35:43 +0500

    105-creating-user-records

--- 04-authentication/server/controllers/authentication.js
+++ 04-authentication/server/controllers/authentication.js
@@ -1,8 +1,28 @@
+const User = require("../models/user");
+
 exports.signup = function (req, res, next) {
-  console.log(req.body);
-  res.send({ success: true });
+  const { email, password } = req.body;
   // see if user with given email exists
-  // if user does exist return an error
-  // if user doesn't exist, create and save user record
-  // respond to request indicating user was created
+  User.findOne({ email: email }, function (err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    // if user does exist return an error
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    // if user doesn't exist, create and save user record
+    const user = new User({ email, password });
+
+    user.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      // respond to request indicating user was created
+      return res.json({ success: true });
+    });
+  });
 };
