c150dbefb0e9a86f69b475a99db0ab454dc261f8
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 09:30:56 +0500

    104-searching-for-users

--- 04-authentication/server/controllers/authentication.js
+++ 04-authentication/server/controllers/authentication.js
@@ -1,3 +1,8 @@
 exports.signup = function (req, res, next) {
+  console.log(req.body);
   res.send({ success: true });
+  // see if user with given email exists
+  // if user does exist return an error
+  // if user doesn't exist, create and save user record
+  // respond to request indicating user was created
 };
