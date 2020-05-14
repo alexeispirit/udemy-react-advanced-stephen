81f777aa719dd8a0e6c2cb26adbfdc2f5afb4ff2
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 11:16:00 +0500

    111-passport-strategies

--- 04-authentication/server/services/passport.js
+++ 04-authentication/server/services/passport.js
@@ -4,3 +4,24 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const User = require("../models/user");
 const config = require("../config");
+
+// setup options for jwt strategy
+const jwtOptions = {};
+
+// create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+  // see if user id in payload exists in database
+  User.findById(payload.sub, function (err, user) {
+    if (err) {
+      return done(err, false);
+    }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// tell passport to use this strategy
