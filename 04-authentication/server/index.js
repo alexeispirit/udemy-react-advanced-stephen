0d8775c03f47a2b38b7fbbb32d860dd02b181026
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 09:25:06 +0500

    103-authentication-controller

--- 04-authentication/server/index.js
+++ 04-authentication/server/index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const router = require("./router");
 
-const app = express();
+// DB Setup
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // App Setup
+const app = express();
 app.use(morgan("combined"));
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
