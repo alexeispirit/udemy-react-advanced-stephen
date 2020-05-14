c125b92d1ab9133d5c807bc47c38cf0017aa7a3c
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 11:24:11 +0500

    113-making-authenticated-request

--- 04-authentication/server/router.js
+++ 04-authentication/server/router.js
@@ -1,5 +1,13 @@
+const passport = require("passport");
+
 const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = function (app) {
+  app.get("/", requireAuth, function (req, res) {
+    res.send("hello");
+  });
   app.post("/signup", Authentication.signup);
 };
