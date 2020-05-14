c2211f40dfc81b89b9cff929d305640db4e663ee
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 22:29:32 +0500

    138-feature-component

--- 04-authentication/client/src/index.js
+++ 04-authentication/client/src/index.js
@@ -9,6 +9,7 @@ import reducers from "./reducers";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import SignUp from "./components/Auth/SignUp";
+import Feature from "./components/Feature";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -18,6 +19,7 @@ ReactDOM.render(
       <App>
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/feature" component={Feature} />
       </App>
     </BrowserRouter>
   </Provider>,
