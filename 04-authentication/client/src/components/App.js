f07584d965a7c423318159087508cdaba217cfeb
Alexei Spirit <alexeispirit@gmail.com>
2020-05-14 14:54:00 +0500

    123-creating-header

--- 04-authentication/client/src/components/App.js
+++ 04-authentication/client/src/components/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import Header from "./Header";
+
 export default () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      App
+    </div>
+  );
 };
